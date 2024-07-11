b3b0edd9cd5f1413e163423515e0cbb688b27f1c
mdaftab41 <mdaftabalam49041@gmail.com>
2024-07-11 17:37:23 +0530

    locationChange

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './component/App.jsx';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
+reportWebVitals();
